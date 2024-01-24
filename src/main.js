c3340a33971f3c2e1f3b1a54dece6d0a86137265
fengstats <feng2860984180@163.com>
2024-01-24 20:35:08 +0800

    feat: 定义内部数据结构

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-import { minToTimeStr, getFileContent } from './utils/index.js'
-
-const inputPath = './index.md'
-
-setup()
-
-function addItem() {}
-
-function run() {}
-
-async function setup() {
-  const content = await getFileContent(inputPath)
-  console.log(content)
-}
-
 export {}
