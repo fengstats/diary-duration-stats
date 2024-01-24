07ae1f5664f7d274afb73b2d3c176a6c855468d5
fengstats <feng2860984180@163.com>
2024-01-24 14:04:25 +0800

    chore: 设计目录结构 / 补充工具函数 / 抽离常量 / 测试使用

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello diary stats script')
+import { minToTimeStr, getFileContent } from './utils.js'
+
+console.log(minToTimeStr(110))
+console.log(await getFileContent('./components/index.tpl'))
+
+export {}
--- utils.js
+++ utils.js
@@ -1,10 +1,7 @@
-// 括号匹配
-const bracketMap = {
-  '': '',
-  '**': '**',
-  '(': ')',
-  '（': '）',
-}
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { BRACKET_MAP } from './constant.js'
 
 // 110 → 01:50
 // `:` 可以通过传入第二个参数替换
@@ -16,11 +13,22 @@ export function minToTime(time, separator = ':') {
 
 // 110 → 01h50min
 // 可以通过传入第二个参数添加括号
-function minToTimeStr(time, bracket = '') {
+export function minToTimeStr(time, bracket = '') {
   if (time === 0) return ''
   const h = Math.floor(time / 60)
   const m = Math.floor(time % 60)
   const hStr = h === 0 ? '' : h + 'h'
   const mStr = m === 0 ? '' : String(m).padStart(2, '0') + 'min'
-  return bracket + hStr + mStr + bracketMap[bracket]
+  return bracket + hStr + mStr + BRACKET_MAP[bracket]
+}
+
+// 相对路径 → 绝对路径
+export function getAbsolutePath(relativePath) {
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  return path.join(currentPath, relativePath)
+}
+
+// 读取文件返回内容
+export async function getFileContent(filePath) {
+  return await fs.readFileSync(getAbsolutePath(filePath), 'utf-8')
 }
